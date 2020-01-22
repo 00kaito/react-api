44bb87aec412d693d2777f0ff845580d337b5dc6
00kaito <bednarz@programmer.net>
2020-01-22 23:34:47 +0100

    Updates

---
+++ precache-manifest.377a74e0db587e31164959413b6d12f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334a5f35d18a974fd10499baa62540aa",
+    "url": "/react-api/index.html"
+  },
+  {
+    "revision": "7042d2df2cd6b144089c",
+    "url": "/react-api/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6f9fea684ec39a8fe641",
+    "url": "/react-api/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7042d2df2cd6b144089c",
+    "url": "/react-api/static/js/2.40971d7c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-api/static/js/2.40971d7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f9fea684ec39a8fe641",
+    "url": "/react-api/static/js/main.3caa9f31.chunk.js"
+  },
+  {
+    "revision": "3a49da904e11bb9c3d6b",
+    "url": "/react-api/static/js/runtime-main.703f2553.js"
+  }
+]);
